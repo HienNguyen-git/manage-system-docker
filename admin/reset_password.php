5625db5c23ac329d53575e41c2af6aaf2255fd1e
haidang2001 <nchdang16012001@gmail.com>
2022-01-05 20:43:03 +0700

    delete bua

--- admin/reset_password.php
+++ admin/reset_password.php
@@ -1,14 +1,5 @@
 <?php 
     require_once('db.php');
-    header('Access-Control-Allow-Origin: *');
-
-    header('Access-Control-Allow-Methods: GET, POST');
-    
-    header("Access-Control-Allow-Headers: X-Requested-With");
-    // $input = json_decode(file_get_contents('php://input'));
-    // $user = $input->username;
-    // echo $user;
-    // die();
 
     if(isset($_GET['username']) && isset($_GET['id'])){
         $user = $_GET['username'];
