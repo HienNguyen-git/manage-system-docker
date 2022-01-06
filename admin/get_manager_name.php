85ba54b68472d1251ff8ec2d518b531085942a65
haidang2001 <nchdang16012001@gmail.com>
2022-01-06 09:27:53 +0700

    main.js

--- admin/get_manager_name.php
+++ admin/get_manager_name.php
@@ -1,8 +1,5 @@
 <?php 
     require_once('db.php');
-    // header('Access-Control-Allow-Origin: *');
-    // header('Access-Control-Allow-Methods: GET, POST');
-    // header("Access-Control-Allow-Headers: X-Requested-With");
 
     if(isset($_GET['department'])){
         $department = $_GET['department'];
